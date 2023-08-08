0d9cb43998cb4a44580210f87bca0002a7ba642e
Sidnei Farias <sidneijr.77@gmail.com>
2023-08-07 22:49:46 -0300

    Inclui spec de teste que verifica se as páginas são carregadas corretamente, via Cypress

--- cypress/e2e/loadPages.cy.ts
+++ cypress/e2e/loadPages.cy.ts
@@ -4,13 +4,28 @@ describe('Verify if pages are accessible', () => {
     cy.log('Home loaded successfully!')
   })
 
-  it('Checkout', () => {
-    cy.visit('/checkout')
-    cy.log('Checkout loaded successfully!')
+  it('Perfil', () => {
+    cy.visit('/perfil')
+    cy.log('Profile loaded successfully!')
   })
 
-  it('Success', () => {
-    cy.visit('/success')
-    cy.log('Success loaded successfully!')
+  it('Login', () => {
+    cy.visit('/login')
+    cy.log('Login loaded successfully!')
+  })
+
+  it('Password Recovery', () => {
+    cy.visit('/password-recovery')
+    cy.log('Password Recovery loaded successfully!')
+  })
+
+  it('Password Feedback', () => {
+    cy.visit('/password-feedback')
+    cy.log('Password Feedback loaded successfully!')
+  })
+
+  it('Agent Selection', () => {
+    cy.visit('/agent-selection')
+    cy.log('Agent Selection loaded successfully!')
   })
 })
