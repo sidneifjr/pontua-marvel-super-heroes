a05af85dae3b48ce0b7b859d22301a6c6286ba5d
Sidnei Farias <sidneijr.77@gmail.com>
2023-08-07 22:26:36 -0300

    Move a chamada das fontes, do global.ts para o index.html

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@100;300;400;500;600;700;800;900&family=Inter:wght@100;300;400;500;600;700;900&display=swap');
-
   * {
     box-sizing: border-box;
   }
