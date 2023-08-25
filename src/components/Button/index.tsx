47fc748e816acd1c9ab9082d622b54eb4aa75bd3
Sidnei Farias <sidneijr.77@gmail.com>
2023-08-25 18:03:12 -0300

    Atualiza props do Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,11 +5,17 @@ interface IButton {
   variant: string
   onClick?: unknown
   type: string
+  'data-cy'?: string
 }
 
 export const Button = (props: IButton) => {
   return (
-    <Btn type={props.type} variant={props.variant} onClick={props.onClick}>
+    <Btn
+      type={props.type}
+      variant={props.variant}
+      onClick={props.onClick}
+      data-cy={props['data-cy']}
+    >
       {props.children}
     </Btn>
   )
