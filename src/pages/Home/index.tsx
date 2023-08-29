9e2e9fb076b3a475d9cb04305c79037476b3eeab
Sidnei Farias <sidneijr.77@gmail.com>
2023-08-29 01:15:48 -0300

    Realiza uma requisição inicial na Home, para exibir conteúdo demonstrativo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,150 +4,104 @@ import {
   HeroListItemInfo,
   HeroListItemInfoDescription,
   HeroListItemInfoTitle,
+  HeroListItemLink,
+  SearchBox,
+  SearchBoxField,
 } from './styles'
 
+import { useForm } from 'react-hook-form'
+
+import { useEffect, useState } from 'react'
 import { Nav } from '../../components/Nav'
-import { SearchBar } from '../../components/SearchBar'
 import { Container } from '../../components/Wrapper/styles'
+import { fetchHeroes } from '../../utils/fetchHeroes'
+
+import { motion } from 'framer-motion'
+import Search from '/icons/search.svg'
+
+const IMAGE_SIZE = 'portrait_medium'
 
 export const HomePage = () => {
+  const [searchResults, setSearchResults] = useState<unknown[]>([])
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      search: '',
+    },
+  })
+
+  const requestData = (param: string, value: string) => {
+    fetchHeroes(param, value as string).then((response) => {
+      const searchedHeroes = response.data.results
+      setSearchResults(searchedHeroes)
+    })
+  }
+
+  const onSubmit = (data: any) => {
+    requestData('get-heroes', data.search as string)
+  }
+
+  // Requisição inicial, somente ao carregar a página.
+  useEffect(() => {
+    requestData('get-heroes', 'a' as string)
+  }, [])
+
+  const heroListing = searchResults?.map((searchResultsItem: any) => {
+    return (
+      <HeroListItem key={searchResultsItem.id}>
+        <motion.div
+          style={{ width: '100%' }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <HeroListItemLink
+            to={`/perfil/${searchResultsItem?.name.toLowerCase()}`}
+          >
+            <img
+              src={`${searchResultsItem.thumbnail.path}/${IMAGE_SIZE}.${searchResultsItem.thumbnail.extension}`}
+              alt="teste"
+            />
+
+            <HeroListItemInfo>
+              <HeroListItemInfoTitle>
+                {searchResultsItem.name}
+              </HeroListItemInfoTitle>
+
+              <HeroListItemInfoDescription>
+                {searchResultsItem.description || 'No description available'}
+              </HeroListItemInfoDescription>
+            </HeroListItemInfo>
+          </HeroListItemLink>
+        </motion.div>
+      </HeroListItem>
+    )
+  })
+
   return (
     <>
       <Nav />
 
       <Container>
-        <SearchBar />
+        <SearchBox onSubmit={handleSubmit(onSubmit)}>
+          <SearchBoxField>
+            <label htmlFor="search">
+              <img src={Search} alt="" />
+            </label>
+
+            <input
+              type="text"
+              placeholder="Busque pelo seu agente"
+              {...register('search')}
+            />
+          </SearchBoxField>
+        </SearchBox>
 
         <HeroList>
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
-
-          <HeroListItem>
-            <img src="" alt="teste" />
-
-            <HeroListItemInfo>
-              <HeroListItemInfoTitle>Wolverine</HeroListItemInfoTitle>
-              <HeroListItemInfoDescription>
-                Born with super-human senses and the power to heal from almost
-                any wound, Wolverine was captured by a secret Canadian
-                organization and given an unbreakable ...
-              </HeroListItemInfoDescription>
-            </HeroListItemInfo>
-          </HeroListItem>
+          {searchResults.length ? (
+            heroListing
+          ) : (
+            <p>No corresponding heroes found. Please try again</p>
+          )}
         </HeroList>
       </Container>
     </>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { styled } from 'styled-components'
 
 export const HeroList = styled.ul`
@@ -14,11 +15,23 @@ export const HeroList = styled.ul`
 `
 
 export const HeroListItem = styled.li`
+  flex-basis: calc(25% - 0.63rem);
   box-shadow: 0px 6px 18px 0px rgba(0, 0, 0, 0.06);
   background: ${(props) => props.theme['gray-100']};
-  flex-basis: calc(25% - 0.63rem);
   flex-grow: 1;
   border-radius: 0.9375rem;
+  display: flex;
+
+  img {
+    width: 5.1875rem;
+    height: 7.4375rem;
+    border-radius: 10px;
+  }
+`
+
+export const HeroListItemLink = styled(Link)`
+  width: 100%;
+  height: 100%;
   padding: 0.88rem 0.63rem;
   display: flex;
   gap: 0.94rem;
@@ -45,4 +58,37 @@ export const HeroListItemInfoDescription = styled.p`
   font-weight: 300;
   letter-spacing: -0.0225rem;
   color: ${(props) => props.theme.black};
+`
+
+export const SearchBox = styled.form`
+  width: 100%;
+`
+
+export const SearchBoxField = styled.fieldset`
+  position: relative;
+  border-bottom: solid 0.0625rem ${(props) => props.theme.divider};
+
+  label {
+    font-size: 0.75rem;
+    line-height: normal;
+    font-weight: 500;
+    letter-spacing: -0.0225rem;
+    color: ${(props) => props.theme['blue-200']};
+    position: absolute;
+    top: 50%;
+    left: 2.31rem;
+    pointer-events: none;
+    transform: translate3d(0, -50%, 0);
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  input {
+    width: 100%;
+    height: 100%;
+    padding: 1.37rem 2rem 1.6rem 4.25rem;
+    border: none;
+    display: block;
+  }
 `
